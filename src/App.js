f6e6c144e145f32003e9a6fc216f01f15d997496
lior <lior.shtivelman@askem.com>
2017-11-05 14:55:36 +0200

    undo changes to repository

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
 import React from 'react';
 import Header from './components/Header';
 import SimpleTable from './components/SimpleTable';
-import AddNewItem from './components/AddNewItem';
-import itemsActions from './actions/itemsActions';
 
-import { connect } from 'react-redux';
+const items = [
+    { id:1, text : "some string", type: "string" },
+    { id:2, text : "35673", type: "number" },
+    { id:3, text : "bla bla bla", type: "string" },
+    { id:4, text : "4.23", type: "float" },
+    { id:5, text : "{id:1, type:2}", type: "object" },
+    { id:6, text : "function(){console.log('hello world!')}", type: "function" },
+    { id:7, text : "sinetsdfjhsd fkjshfksdf", type: "string" },
+    { id:8, text : "sinetsdfjhsd fkjshfksdf", type: "string" },
+    { id:9, text : "mcmcnmvncmvcv", type: "string" },
+    { id:10, text : "234d%dfgg(*&(%d", type: "string" },
+    { id:11, text : "123.4", type: "float" },
+    { id:12, text : "30.5%", type: "string" },
+]
+
 
 class App extends React.Component {
     render() {
         return (
             <div>
                 <Header headerText="This is sample"/>  
-                <AddNewItem addNewItem={ this.props.addNewItem }/>              
-                <SimpleTable items={ this.props.items } />
+                <SimpleTable items={ items } />
             </div>
         )
     }
 }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        addNewItem : (newItem) => { dispatch(itemsActions.addNewItem(newItem))}
-    }
-}
-
-const mapStateToProps = (state) => {
-    return {
-        items : state.items
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,30 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { AppContainer } from 'react-hot-loader';
 import './mainStyle.scss';
-import { createStore,  applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux';
-import mainReducer from './reducers/mainReducer';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const enhancer = composeEnhancers(
-  //applyMiddleware(...middleware),
-);
-
-const store = createStore(
-    mainReducer,
-    enhancer
-);
-
-/*
-<AppContainer>
-<App />
-</AppContainer>
-*/
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+        <AppContainer>
+            <App />
+        </AppContainer>    
     , document.querySelector('#root')
 );
     

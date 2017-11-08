156b7c171679e5f819425969595dcaae5f30728d
lior <lior.shtivelman@askem.com>
2017-11-08 17:31:32 +0200

    changes to eslint

--- webpack.config.js
+++ webpack.config.js
@@ -2,41 +2,40 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    entry : [
+    entry: [
         'react-hot-loader/patch',
         'babel-polyfill',
         'webpack-dev-server/client?http://0.0.0.0:5000',
         'webpack/hot/only-dev-server',
-        path.resolve(__dirname + '/src/index.js')
+        path.resolve(__dirname, '/src/index.js'),
     ],
-    output : {
-        path : path.resolve(__dirname + '/build'),
-        filename : 'bundle.js',
-        publicPath : '/'
+    output: {
+        path: path.resolve(__dirname, '/build'),
+        filename: 'bundle.js',
+        publicPath: '/',
     },
     devServer: {
         contentBase: './build',
-        hot: true
+        hot: true,
     },
-    module : {
-        loaders : [
-            { 
-                test:  /\.(js|jsx)$/,
-                exclude: /node_modules/, 
+    module: {
+        loaders: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
                 loader: 'babel-loader',
-                query : {
-                    presets : [ 'react', 'es2015' ]
-                }
+                query: {
+                    presets: ['react', 'es2015'],
+                },
             },
             {
-                test : /\.scss$/,
-                loader : [ 'style-loader', 'css-loader', 'sass-loader']
+                test: /\.scss$/,
+                loader: ['style-loader', 'css-loader', 'sass-loader'],
             },
         ]
     },
-    plugins : [
+    plugins: [
         new webpack.NamedModulesPlugin(),
         new webpack.HotModuleReplacementPlugin()
     ]
-
 }
